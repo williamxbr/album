813e48fc4a996c19c4866ab90186a958de875ad0
William <williamxbr@gmail.com>
2018-06-12 21:48:14 -0300

    Add files via upload

--- app/Eniwine.Album.Copa.Dominio/Interfaces/Aplicacao/ITrocaAplicacao.cs
+++ app/Eniwine.Album.Copa.Dominio/Interfaces/Aplicacao/ITrocaAplicacao.cs
@@ -4,5 +4,8 @@ namespace Eniwine.Album.Copa.Dominio.Interfaces.Aplicacao
 {
     public interface ITrocaAplicacao : IAplicacaoBase<Troca>
     {
+        void solicitar(Troca troca);
+        void aceitar(Troca troca);
+        void recusar(Troca troca);
     }
 }
---
+++ app/Eniwine.Album.Copa.Web/Controllers/FigurinhaController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Eniwine.Album.Copa.Web.Controllers
+{
+    public class FigurinhaController : Controller
+    {
+        // GET: Figurinha
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Figurinhas()
+        {
+            return View();
+        }
+
+
+    }
+}
